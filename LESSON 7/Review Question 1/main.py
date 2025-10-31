f07673b77498012009cab750e50276c3882aa61a
KhylleVillasurda <freakyvader@proton.me>
2025-10-31 15:23:52 +0800

    LESSON 7
    Status: Done

---
+++ LESSON 7/Review Question 1/main.py
@@ -0,0 +1,3 @@
+import greeter as greet
+greet = greet.greet("Khylle!")
+print(greet)
---
+++ LESSON 7/Review Question 2/main.py
@@ -0,0 +1,6 @@
+from Helpers.string import shout
+from Helpers.math import area
+
+total = area(5, 8)
+
+print(shout(f"The area of a 5-b-8 is {total}"))
